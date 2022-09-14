470cc39e782d8d9c836c1973f5cf29cfbe96a20e
AlderaminCph <sypkova.anastasia@yandex.ru>
2022-09-14 23:00:15 +0300

    tests for distance value between two arrays

---
+++ tests/day3/test_distance.py
@@ -0,0 +1,14 @@
+import pytest
+from day3.distance_value_between_two_arrays import findTheDistanceValue
+
+
+@pytest.mark.parametrize(
+    ["arr1", "arr2", "d", "expected_result"],
+    [
+        ([4, 5, 8], [10, 9, 1, 8], 2, 2),
+        ([1, 4, 2, 3], [-4, -3, 6, 10, 20, 30], 3, 2),
+        ([2, 1, 100, 3], [-5, -2, 10, -3, 7], 6, 1),
+    ],
+)
+def test_distance(arr1, arr2, d, expected_result):
+    assert expected_result == findTheDistanceValue(arr1, arr2, d)
