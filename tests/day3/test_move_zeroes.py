cf52450ed1bda32ddd1842da57c14aaf2aee2114
AlderaminCph <sypkova.anastasia@yandex.ru>
2022-09-14 23:22:09 +0300

    move zeroes

---
+++ tests/day3/test_move_zeroes.py
@@ -0,0 +1,10 @@
+import pytest
+from day3.move_zeroes import moveZeroes
+
+
+@pytest.mark.parametrize(
+    ["nums", "expected_result"],
+    [([0, 1, 0, 3, 12], [1, 3, 12, 0, 0]), ([0], [0])],
+)
+def test_move_zeroes(nums, expected_result):
+    assert expected_result == moveZeroes(nums)
