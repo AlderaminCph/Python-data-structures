0bd7ac0adfbac48234fbb61476a29a3c54e1c876
AlderaminCph <sypkova.anastasia@yandex.ru>
2022-09-14 21:43:43 +0300

    valid perfect square

---
+++ day3/valid_perfect_square.py
@@ -0,0 +1,39 @@
+"""
+Given a positive integer num, write a function which returns
+True if num is a perfect square else False.
+
+Follow up: Do not use any built-in library function such as sqrt.
+
+
+
+Example 1:
+
+Input: num = 16
+Output: true
+
+Example 2:
+
+Input: num = 14
+Output: false
+
+"""
+
+
+def isPerfectSquare(num):
+    """
+    :type num: int
+    :rtype: bool
+    """
+    left = 1
+    right = num
+
+    while left < right:
+        mid = int(left + (right - left) / 2)
+        sq_mid = mid * mid
+        if sq_mid == num:
+            return True
+        elif sq_mid > num:
+            right = mid
+        else:
+            left = mid + 1
+    return False
