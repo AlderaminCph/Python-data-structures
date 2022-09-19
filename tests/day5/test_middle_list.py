630ceb299b6b421844b7c002f45bedd5c40c2ed2
AlderaminCph <sypkova.anastasia@yandex.ru>
2022-09-19 12:39:45 +0300

    tests for middle of the linked list were written

---
+++ tests/day5/test_middle_list.py
@@ -0,0 +1,19 @@
+import pytest
+
+from day5.middle_of_the_linked_list import LinkedList, middleNode
+
+
+@pytest.mark.parametrize(
+    ["listvalues", "expected"],
+    [([1, 2, 3, 4, 5], [3, 4, 5]), ([1, 2, 3, 4, 5, 6], [4, 5, 6])],
+)
+def test_middleNode(listvalues, expected):
+    llist = LinkedList()
+    for i in listvalues[::-1]:
+        llist.push(i)
+    node = middleNode(llist.head)
+    res = []
+    while node:
+        res.append(node.val)
+        node = node.next
+    assert expected == res
