6e432a58e7cf2f963237cf33353e70c4422f04d8
AlderaminCph <sypkova.anastasia@yandex.ru>
2022-09-21 00:26:20 +0300

    permutations in a string

---
+++ day6/permutation_in_string.py
@@ -0,0 +1,34 @@
+"""
+567. Permutation in String
+
+Given two strings s1 and s2,
+return true if s2 contains a permutation of s1, or false otherwise.
+
+In other words, return true if one
+of s1's permutations is the substring of s2.
+
+
+
+Example 1:
+
+>>> checkInclusion("ab","eidbaooo")
+true
+
+Explanation: s2 contains one permutation of s1 ("ba").
+
+Example 2:
+>>> checkInclusion("ab","eidboaoo")
+false
+
+"""
+
+import doctest
+
+
+def checkInclusion(s1: str, s2: str) -> bool:
+    """
+    Solution Idea:
+    """
+
+
+doctest.testmod()
