a97955a78d8bddafe8878a67e4de278ecd451eeb
AlderaminCph <sypkova.anastasia@yandex.ru>
2022-09-22 22:41:18 +0300

    rotten oranges written

--- day9/rotting_oranges.py
+++ day9/rotting_oranges.py
@@ -13,25 +13,26 @@ orange becomes rotten.
 Return the minimum number of minutes that must elapse until no cell has a fresh
 orange. If this is impossible, return -1.
 
-Input: grid = [[2,1,1],[1,1,0],[0,1,1]]
-Output: 4
+>>> orangesRotting([[2,1,1],[1,1,0],[0,1,1]])
+4
 
 Example 2:
 
-Input: grid = [[2,1,1],[0,1,1],[1,0,1]]
-Output: -1
+>>> orangesRotting([2,1,1],[0,1,1],[1,0,1]])
+-1
 Explanation: The orange in the bottom left corner (row 2, column 0) is never
 rotten, because rotting only happens 4-directionally.
 
 Example 3:
 
-Input: grid = [[0,2]]
-Output: 0
+>>> orangesRotting([[0,2]])
+0
 Explanation: Since there are already no fresh oranges at minute 0, the answer
 is just 0.
 
 """
 from typing import List
+from collections import deque
 import doctest
 
 
@@ -40,6 +41,38 @@ class Solution:
         """
         Solution
         """
+        q = deque()
+        time, fresh = 0, 0
+
+        ROWS, COLS = len(grid), len(grid[0])
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == 1:  # count the number of fresh oranges
+                    fresh += 1
+                if grid[r][c] == 2:  # add every rotting orange to the queue
+                    q.append([r, c])
+        directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+        while q and fresh > 0:
+            for i in range(len(q)):
+                r, c = q.popleft()  # dlete rotting orange from queue
+                for dr, dc in directions:
+                    row = r + dr
+                    col = c + dc
+                    # if in bounds and fresh, make rotten
+                    if (
+                        row < 0
+                        or row == ROWS
+                        or col < 0
+                        or col == COLS
+                        or grid[row][col] != 1
+                    ):
+                        continue
+                    grid[row][col] == 2  # make orange rotten
+                    fresh -= 1  # decrement the number of fresh oranges
+                    q.append([row][col])
+            time += 1
+        return time if fresh == 0 else -1
 
 
 doctest.testmod()
