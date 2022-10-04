be3f250ce347ade2f7d4fbbd2ba38e48a640d3b4
AlderaminCph <sypkova.anastasia@yandex.ru>
2022-10-04 22:43:47 +0300

    tests for recursively reversed list

---
+++ day10/recursively_reversed_list.py
@@ -0,0 +1,36 @@
+"""
+Reverse a Link list recursively
+"""
+from typing import Optional
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def push(self, val):
+        new_node = ListNode(val)
+        new_node.next = self.head
+        self.head = new_node
+
+
+class Solution:
+    # recursive Solution
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head:
+            return None
+
+        newHead = head
+        if head.next:
+            newHead = self.reverseList(head.next)
+            head.next.next = head
+        head.next = None
+
+        return newHead
---
+++ tests/day10/test_recursive_reverse_linked_list.py
@@ -0,0 +1,20 @@
+import pytest
+from day10.recursively_reversed_list import Solution, LinkedList
+from typing import List
+
+
+@pytest.mark.parametrize(
+    ["initial_list", "reverse_list"],
+    [([1, 2, 3, 4, 5], [5, 4, 3, 2, 1]), ([1, 2], [2, 1]), ([], [])],
+)
+def test_reverse_list(initial_list: List[int], reverse_list: List[int]):
+    initial = LinkedList()
+    for item in initial_list[::-1]:
+        initial.push(item)
+    s = Solution()
+    curr = s.reverseList(initial.head)
+    res = []
+    while curr:
+        res.append(curr.val)
+        curr = curr.next
+    assert res == reverse_list
