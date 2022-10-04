b3bb00b191a6935ce6f39a273e54b9a049330397
AlderaminCph <sypkova.anastasia@yandex.ru>
2022-10-05 00:13:26 +0300

    letter case permutation

---
+++ day11_recursion_backtracking/letter_case_permutation.py
@@ -0,0 +1,27 @@
+"""
+Given a string s, you can transform every letter individually to be lowercase
+or uppercase to create another string.
+
+Return a list of all possible strings we could create. Return the output in
+any order.
+
+Example 1:
+>>> s = Solution()
+>>> s.letterCasePermutation("a1b2")
+["a1b2", "a1B2", "A1b2", "A1B2"]
+
+Example 2:
+>>> s.letterCasePermutation(""3z4"")
+["3z4", "3Z4"]
+
+"""
+from typing import List
+import doctest
+
+
+class Solution:
+    def letterCasePermutation(self, s: str) -> List[str]:
+        """ """
+
+
+doctest.testmod()
