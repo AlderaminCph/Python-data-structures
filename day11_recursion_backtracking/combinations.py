1bf3444a723c2e854eb100ec41850399a80a4f44
AlderaminCph <sypkova.anastasia@yandex.ru>
2022-10-04 23:24:54 +0300

    doctests for combination

--- day11_recursion_backtracking/combinations.py
+++ day11_recursion_backtracking/combinations.py
@@ -9,7 +9,10 @@ You may return the answer in any order.
 Example 1:
 
 Input: n = 4, k = 2
-Output: [[1,2],[1,3],[1,4],[2,3],[2,4],[3,4]]
+>>> s = Solution()
+>>> s.combine(4, 2)
+[[1, 2], [1, 3], [1, 4], [2, 3], [2, 4], [3, 4]]
+
 Explanation: There are 4 choose 2 = 6 total combinations.
 Note that combinations are unordered, i.e., [1,2] and [2,1] are considered to
 be the same combination.
@@ -17,10 +20,13 @@ be the same combination.
 Example 2:
 
 Input: n = 1, k = 1
-Output: [[1]]
+>>> s.combine(1, 1)
+[[1]]
+
 Explanation: There is 1 choose 1 = 1 total combination.
 
 """
+import doctest
 from typing import List
 
 
@@ -40,3 +46,6 @@ class Solution:
 
         backtrack(1, [])
         return result
+
+
+doctest.testmod()
